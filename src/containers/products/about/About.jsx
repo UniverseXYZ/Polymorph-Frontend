c4a72d698057190e31245d4a56826457af1e9476
gope-gope <georgigeorgiew97@gmail.com>
2022-04-12 12:11:40 +0300

    Removed components from About page

--- src/containers/products/about/About.jsx
+++ src/containers/products/about/About.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-<<<<<<< HEAD
-=======
-import Welcome from '../../../components/products/about/Welcome.jsx';
-import OurTeam from '../../../components/products/about/OurTeam.jsx';
->>>>>>> 19e37949 (Removed a chunk of auctions-related functionalities)
 import Head from 'next/head';
 import { useThemeStore } from 'src/stores/themeStore';
 import OpenGraphImage from '@assets/images/open-graph/about-us.png';
@@ -21,11 +16,6 @@ const About = () => {
         description={'Mint single or multiple NFTs, create and edit NFT Collections, and run auctions with multiple NFTs per winner. In this Universe anything is possible.'}
         image={OpenGraphImage}
       />
-<<<<<<< HEAD
-=======
-      <Welcome />
-      <OurTeam />
->>>>>>> 19e37949 (Removed a chunk of auctions-related functionalities)
     </div>
   );
 };
