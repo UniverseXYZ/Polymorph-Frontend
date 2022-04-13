d4d9d20668893f877b427fc269288e2763cf603d
gope-gope <georgigeorgiew97@gmail.com>
2022-04-13 09:57:22 +0300

    Removed redundant popups and related components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -68,7 +68,6 @@ import '../src/components/polymorphs/scramble/styles/PolymorphScrambleProp.scss'
 import '../src/components/select/SelectComponent.scss';
 
 import '../src/components/badge/Badge.scss';
-import '../src/components/popups/transferNFT/TransferNFTPopup.scss';
 import '../src/app/modules/account/pages/my-nfts-page/components/search-filters/search-filters/SearchFilters.scss';
 import '../src/app/modules/account/pages/my-nfts-page/components/search-filters/search-nft-filed/SearchField.scss';
 import '../src/app/modules/nft/pages/nft-page/components/nft-info/components/tab-properties/TabProperties.scss';
--- src/components/popups/transferNFT/constants/steps.js
+++
@@ -1,6 +0,0 @@
-// TODO: make as Enum in ts
-export const Steps = {
-  Form: 1,
-  Loading: 2,
-  Success: 3,
-};
