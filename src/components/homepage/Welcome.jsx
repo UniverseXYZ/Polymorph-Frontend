6410be644b2ef71c339d0d352eca6bd6244bbe34
gope-gope <georgigeorgiew97@gmail.com>
2022-04-26 20:30:35 +0300

    Implemented custom carousel

--- src/components/homepage/Welcome.jsx
+++ src/components/homepage/Welcome.jsx
@@ -8,6 +8,7 @@ import heroVideo from '../../assets/images/hero_video.mp4';
 import Button from '../button/Button.jsx';
 import SubscribePopup from '../popups/SubscribePopup.jsx';
 import SlickCarousel from './SlickCarousel';
+import TestCarousel from './TestCarousel'
 
 const Welcome = () => {
   const [loaded, setLoaded] = useState(false);
