9197a41f26aa9d05f880f61a2472a00b6081eb90
VahanMuradyan <10vahan.m16@gmail.com>
2022-05-05 18:19:16 +0400

    Implement burning animation in Burn Polymorph [sc-3455]

--- src/containers/burnPolymorphs/BurnBatchPolymorphs.tsx
+++ src/containers/burnPolymorphs/BurnBatchPolymorphs.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import polymorphImg from '../../assets/images/soldier.png';
+import polymorphImg from '../../assets/images/burn-polymorph.png';
 import BurnPolymorphs from '../../components/burnPolymorphs/BurnPolymorphs';
 
 export const BurnBatchPolymorphs = () => {
@@ -32,51 +32,7 @@ export const BurnBatchPolymorphs = () => {
     {
       id: 7,
       polymorphImg: polymorphImg
-    },
-    {
-      id: 8,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 9,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 10,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 11,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 12,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 13,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 14,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 15,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 16,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 17,
-      polymorphImg: polymorphImg
-    },
-    {
-      id: 18,
-      polymorphImg: polymorphImg
-    },
+    }
   ];
 
   return (
