91a467859b472ab3d36194f18c4521c4913bed6f
gope-gope <88503390+gope-gope@users.noreply.github.com>
2022-05-11 17:05:13 +0300

    Feature/b2m display selected polymorphs (#31)
    * ImageURLs added to store. Display selected polymorphs in burn batch page.
    
    * Changed prop name to remove warning in console
    
    * Wired up the back button functionality
    
    * Added key prop to mapping
    
    * Added single polymorph burn view

--- src/containers/burnPolymorphs/BurnBatchPolymorphs.tsx
+++ src/containers/burnPolymorphs/BurnBatchPolymorphs.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import polymorphImg from '../../assets/images/burn-polymorph.png';
 import BurnPolymorphs from '../../components/burnPolymorphs/BurnPolymorphs';
 import { usePolymorphStore } from 'src/stores/polymorphStore';
 
 export const BurnBatchPolymorphs = () => {
   const { userSelectedPolymorphsToBurn } = usePolymorphStore();
 
-  const burnPolymorphsArr = userSelectedPolymorphsToBurn.map((index) => ({ id: index, polymorphImg: polymorphImg }));
-
-  return <BurnPolymorphs characters={burnPolymorphsArr} type="batch" />;
+  return <BurnPolymorphs characters={userSelectedPolymorphsToBurn} type="batch" />;
 };
