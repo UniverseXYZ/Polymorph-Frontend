4904f607b8b1b9ae9d6ef25b030a3aead632a0c4
gope-gope <88503390+gope-gope@users.noreply.github.com>
2022-05-13 10:44:29 +0300

    Feature/my polymorphs card design (#37)
    * Adjusted design of cards in my-polymorphs
    
    * Adjusted z-index of sticky bar interfering with header dropdown
    
    * Added padding to my-nfts container
    
    * Added a variable in .env containing link to polymorph on marketplace
    
    * Fixed hardcoded link in polymorph cards

--- src/components/rarityCharts/list/MyPolymorphCard.jsx
+++ src/components/rarityCharts/list/MyPolymorphCard.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import Popup from 'reactjs-popup';
-import RarityRankPopup from '../../popups/RarityRankPopup.jsx';
-import { getPolymorphMeta } from '../../../utils/api/polymorphs.js';
-import { renderLoaderWithData } from '../../../containers/rarityCharts/renderLoaders.js';
-import { useRouter } from 'next/router';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Popup from "reactjs-popup";
+import RarityRankPopup from "../../popups/RarityRankPopup.jsx";
+import { getPolymorphMeta } from "../../../utils/api/polymorphs.js";
+import { renderLoaderWithData } from "../../../containers/rarityCharts/renderLoaders.js";
+import { useRouter } from "next/router";
+import ThreeDotsSVG from "../../../assets/images/three-dots-horizontal.svg";
+import LinkOut from "../../../assets/images/burn-to-mint-images/link-out.svg";
+
+const marketplaceLinkOut =
+  process.env.REACT_APP_LINK_TO_POLYMORPH_IN_MARKETPLACE;
 
 const MyPolymorphCard = ({ item }) => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   const fetchMetadata = async () => {
     setLoading(true);
     const data = await getPolymorphMeta(item.tokenid);
@@ -36,8 +43,33 @@ const MyPolymorphCard = ({ item }) => {
         />
       </div>
       <div className="card--footer">
-        <h2>{item.character}</h2>
-        <p>{`ID: #${item.tokenid}`}</p>
+        <div className="card--footer--top">
+          <h2>{item.character}</h2>
+        </div>
+        <div className="card--footer--bottom">
+          <span>{`ID: ${item.tokenid}`}</span>
+          <button
+            onClick={(event) => {
+              event.stopPropagation();
+              setDropdownOpen(!dropdownOpen);
+            }}
+          >
+            <img src={ThreeDotsSVG} />
+          </button>
+        </div>
+        {dropdownOpen ? (
+          <div className={"dropdown"}>
+            <button
+              onClick={(event) => {
+                event.stopPropagation();
+                window.open(`${marketplaceLinkOut}/${item.tokenid}`);
+              }}
+            >
+              <img src={LinkOut} />
+              View on marketplace
+            </button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
