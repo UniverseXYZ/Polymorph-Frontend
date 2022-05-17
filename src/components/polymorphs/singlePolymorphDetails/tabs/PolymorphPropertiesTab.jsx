c8eb89b7c5de13d086dffa07e9950f3503fa698b
gope-gope <88503390+gope-gope@users.noreply.github.com>
2022-05-17 12:48:55 +0300

    Feature/single polymorph view properties (#48)
    * Display name, title, image
    
    * Display data in properties tab
    
    * Hide buttons for non-owner users viewing a polymorph
    
    * Added key to mapping. Adjusted property name for RarityRankPopupProperty

--- src/components/polymorphs/singlePolymorphDetails/tabs/PolymorphPropertiesTab.jsx
+++ src/components/polymorphs/singlePolymorphDetails/tabs/PolymorphPropertiesTab.jsx
@@ -1,93 +1,129 @@
-import React, {useEffect} from 'react';
-import PolymorphPropertyCard from '../../polymorphPropertyCard/PolymorphPropertyCard';
+import React, { useEffect, useState } from "react";
+import GeneParser from "@legacy/helpers/GeneParser";
+import RarityRankPopupProperty from "@legacy/popups/RarityRankPopupProperty";
 
-const PolymorphPropertiesTab = () => {
-  // Dummy data
-  const polymorphProperties = [
+const PolymorphPropertiesTab = ({ data }) => {
+  const [traitsMap, setTraitsMap] = useState(
+    GeneParser.parse(data.currentgene)
+  );
+
+  const properties = [
     {
-      id: 1,
-      text1: 'Base character',
-      text2: 'Troll God',
-      text3: '28% have this trait',
-      type: 'gradient',
+      propertyName: "Character",
+      value: data.character,
+      mainMatchingAttributes: data.mainmatchingtraits,
+      secMatchingAttributes: data.secmatchingtraits,
+      genesMap: traitsMap,
+      matchingHands: data.matchinghands,
     },
     {
-      id: 2,
-      text1: 'Headwear',
-      text2: 'Golden Spartan Helmet',
-      text3: '58% have this trait',
-      type: 'blue',
+      propertyName: "Footwear",
+      value: data.footwear,
+      mainMatchingAttributes: data.mainmatchingtraits,
+      secMatchingAttributes: data.secmatchingtraits,
+      genesMap: traitsMap,
+      matchingHands: data.matchinghands,
     },
     {
-      id: 3,
-      text1: 'Torso',
-      text2: 'Silver Spartan Armor',
-      text3: '58% have this trait',
-      type: 'blue',
+      propertyName: "Pants",
+      value: data.pants,
+      mainMatchingAttributes: data.mainmatchingtraits,
+      secMatchingAttributes: data.secmatchingtraits,
+      genesMap: traitsMap,
+      matchingHands: data.matchinghands,
     },
     {
-      id: 4,
-      text1: 'Footwear',
-      text2: 'Brown Spartan Sandals',
-      text3: '58% have this trait',
-      type: 'pink',
+      propertyName: "Torso",
+      value: data.torso,
+      mainMatchingAttributes: data.mainmatchingtraits,
+      secMatchingAttributes: data.secmatchingtraits,
+      genesMap: traitsMap,
+      matchingHands: data.matchinghands,
     },
     {
-      id: 5,
-      text1: 'Left-hand accessory',
-      text2: 'Bow & Arrow',
-      text3: '58% have this trait',
-      type: '',
+      propertyName: "Eyewear",
+      value: data.eyewear,
+      mainMatchingAttributes: data.mainmatchingtraits,
+      secMatchingAttributes: data.secmatchingtraits,
+      genesMap: traitsMap,
+      matchingHands: data.matchinghands,
     },
     {
-      id: 6,
-      text1: 'Right -hand accessory',
-      text2: 'Silver Spartan Sword',
-      text3: '58% have this trait',
-      type: 'pink',
+      propertyName: "Headwear",
+      value: data.headwear,
+      mainMatchingAttributes: data.mainmatchingtraits,
+      secMatchingAttributes: data.secmatchingtraits,
+      genesMap: traitsMap,
+      matchingHands: data.matchinghands,
     },
     {
-      id: 7,
-      text1: 'Footwear',
-      text2: 'Brown Spartan Sandals',
-      text3: '58% have this trait',
-      type: 'pink',
+      propertyName: "Left Hand",
+      value: data.lefthand,
+      mainMatchingAttributes: data.mainmatchingtraits,
+      secMatchingAttributes: data.secmatchingtraits,
+      genesMap: traitsMap,
+      matchingHands: data.matchinghands,
     },
     {
-      id: 8,
-      text1: 'Torso',
-      text2: 'Silver Spartan Armor',
-      text3: '58% have this trait',
-      type: 'blue',
+      propertyName: "Right Hand",
+      value: data.righthand,
+      mainMatchingAttributes: data.mainmatchingtraits,
+      secMatchingAttributes: data.secmatchingtraits,
+      genesMap: traitsMap,
+      matchingHands: data.matchinghands,
     },
-  ]
+    {
+      propertyName: "Background",
+      value: data.background,
+      mainMatchingAttributes: data.mainmatchingtraits,
+      secMatchingAttributes: data.secmatchingtraits,
+      genesMap: traitsMap,
+      matchingHands: data.matchinghands,
+    },
+  ];
 
   useEffect(() => {
-    let tooltips = document.querySelectorAll('.polymorph--property--card .tooltiptext');
-    if(tooltips.length) {
-      tooltips.forEach(t => {
+    let tooltips = document.querySelectorAll(
+      ".polymorph--property--card .tooltiptext"
+    );
+    if (tooltips.length) {
+      tooltips.forEach((t) => {
         let elRect = t.getBoundingClientRect();
         let pos = window.innerWidth - elRect.right;
-        if(pos < 0) {
-          t.classList.add('align--right');
+        if (pos < 0) {
+          t.classList.add("align--right");
         }
-      })
+      });
     }
-  }, [])
+  }, []);
 
   return (
-    <div className='polymorph--properties--tab'>
-      <div className='polymorph--tags'>
-        <span>Rank: <b>#1</b></span>
-        <span>Rarity score: <b>2400</b></span>
+    <div className="polymorph--properties--tab">
+      <div className="polymorph--tags">
+        <span>
+          Rank: <b>{data.rank}</b>
+        </span>
+        <span>
+          Rarity score: <b>{data.rarityscore}</b>
+        </span>
       </div>
-      <div className='polymorph--properties--grid'>
-        {polymorphProperties.map(prop => (
-          <PolymorphPropertyCard key={prop.id} property={prop} />
-        ))}
+      <div className="polymorph--properties--grid">
+        {properties.map((prop) => {
+          return (
+            <RarityRankPopupProperty
+              key={prop.propertyName}
+              propertyName={prop.propertyName}
+              value={prop.value}
+              mainMatchingAttributes={prop.mainMatchingAttributes}
+              secMatchingAttributes={prop.secMatchingAttributes}
+              genesMap={traitsMap}
+              matchingHands={data.matchinghands}
+            />
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PolymorphPropertiesTab
+export default PolymorphPropertiesTab;
