61d4effd67c1472a89f4516acc6d8e1c6b2e67b8
VahanMuradyan <10vahan.m16@gmail.com>
2022-05-17 12:31:39 +0400

    Polymorphs home page [sc-4228] (#43)

--- src/components/polymorphUniverse/latestFeaturesSection/LatestFeaturesSection.jsx
+++ src/components/polymorphUniverse/latestFeaturesSection/LatestFeaturesSection.jsx
@@ -7,6 +7,7 @@ import battleUniverseIcon from '../../../assets/images/battle-universe-icon.svg'
 import burnToMintIcon from '../../../assets/images/burn-to-mint-icon.svg';
 import Button from '@legacy/button/Button';
 import PolymorphicFaces from '../../../assets/images/polymorphic-faces.png'
+import ArrowLeftIcon from '@legacy/svgs/ArrowLeftIcon';
 
 const LatestFeaturesSection = () => {
   const router = useRouter();
@@ -18,41 +19,41 @@ const LatestFeaturesSection = () => {
         <div className="grid">
           <div>
             <img src={rarityChartIcon} alt="Rarity chart" />
-            <h2>Rarity chart</h2>
+            <h2>Rarity Chart</h2>
             <p>
               Compare your Polymorph’s ranking, scoring and information based on the rarity of its
               traits.
             </p>
             <button className='light-border-button' onClick={() => router.push('/polymorph-rarity')}>
               Explore
-              <img src={arrowRight} alt="Arrow right" />
+              <ArrowLeftIcon />
             </button>
           </div>
           <div>
             <img src={burnToMintIcon} alt="Burn to mint" />
-            <h2 className="burn-mint-h2">Burn to mint</h2>
+            <h2 className="burn-mint-h2">Burn to Mint</h2>
             <p>
               Say goodbye to the old version of your Polymorph forever and hello to your shiny new
               one.
             </p>
             <button className='light-border-button' onClick={() => router.push('/burn-to-mint')}>
               Explore
-              <img src={arrowRight} alt="Arrow right" />
+              <ArrowLeftIcon />
             </button>
           </div>
           <div>
             <img src={battleUniverseIcon} alt="Battle universe" />
-            <h2>Battle universe</h2>
+            <h2>Battle Universe</h2>
             <p>Wager ETH and compete against other Polymorph owners around the world.</p>
             <button className='button-disabled'>
               Coming Soon
-              {/* <img src={arrowRight} alt="Arrow right" /> */}
+              {/* <ArrowLeftIcon /> */}
             </button>
           </div>
         </div>
       </div>
       <div className='polymorphic--faces--section--container'>
-        <img src={PolymorphicFaces}/>
+        <img src={PolymorphicFaces} alt="" />
         <div>
           <h2>Claim Your Polymorphic Faces for Free</h2>
           <p>
