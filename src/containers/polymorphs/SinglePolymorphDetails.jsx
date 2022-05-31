b12272484b4c620c1dca762875477d6c9180fdd9
gope-gope <88503390+gope-gope@users.noreply.github.com>
2022-05-31 14:27:22 +0300

    Feature/iframe (#79)
    * Implemented iframes for V2 polymorphs in view single polymorph details
    
    * Fetched iframe from metadata v2

--- src/containers/polymorphs/SinglePolymorphDetails.jsx
+++ src/containers/polymorphs/SinglePolymorphDetails.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImageWithBadges from "../../components/polymorphs/singlePolymorphDetails/ImageWithBadges";
 import DetailsWithTabs from "../../components/polymorphs/singlePolymorphDetails/DetailsWithTabs";
 import { useRouter } from "next/router";
 import { useSearchPolymorphs } from "@legacy/hooks/useMyNftsRarityDebouncerAll";
 import LoadingSpinner from "../../components/svgs/LoadingSpinner.jsx";
+import { usePolymorphStore } from "src/stores/polymorphStore";
+import { getPolymorphMetaV2 } from "@legacy/api/polymorphs";
 
 export const SinglePolymorphDetails = () => {
   const router = useRouter();
+  const [iframeData, setIframeData] = useState();
+  const [isV1, setIsV1] = useState();
+
+  const { userPolymorphs } = usePolymorphStore();
 
   const { results } = useSearchPolymorphs();
   const polymorphId = router.query.id;
@@ -14,14 +20,37 @@ export const SinglePolymorphDetails = () => {
     (result) => polymorphId === result.tokenid.toString()
   );
 
+  useEffect(() => {
+    const polymorphIsV1 = userPolymorphs.some(
+      (userPolymorph) => userPolymorph.tokenId === polymorphId
+    );
+    if (polymorphIsV1) {
+      setIsV1(true);
+    } else {
+      setIsV1(false);
+    }
+  }, []);
+
+  useEffect(async () => {
+    try {
+      const { data } = await getPolymorphMetaV2(polymorphId);
+      setIframeData(data.animation_url);
+    } catch (err) {
+      console.log(err);
+    }
+  }, [isV1, results]);
+
   return (
     <>
       {polymorphMetadata.length ? (
         <div className="single--polymorph--details--page">
           <>
+            {/* IF V1 */}
             <ImageWithBadges
               polymorphId={polymorphId}
               polymorphData={polymorphMetadata[0]}
+              isV1={isV1}
+              iframeData={iframeData}
             />
             <DetailsWithTabs
               polymorphId={polymorphId}
