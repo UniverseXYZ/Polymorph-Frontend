592bb0b68c86b6bd1020851788cefe602f9ef8db
gope-gope <88503390+gope-gope@users.noreply.github.com>
2022-06-01 15:18:02 +0300

    Georgiivaylov/sc 4640 (#87)
    * Added animation files
    
    * Implemented animations

--- src/components/rarityCharts/welcome/Welcome.jsx
+++ src/components/rarityCharts/welcome/Welcome.jsx
@@ -1,39 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useWindowSize } from "react-use";
 import WelcomeWrapper from "../../ui-elements/WelcomeWrapper";
+import Lottie from "react-lottie";
+import animationDataDesktop from "../../../utils/animations/rarity_header_animation_desktop.json";
+import animationDataTablet from "../../../utils/animations/rarity_header_animation_tablet.json";
+import animationDataMobile from "../../../utils/animations/rarity_header_animation_mobile.json";
 // import './Welcome.scss';
 
-const marquee = () => (
-  <p>
-    <span className="marquee--text--polymorph">POLYMORPH</span>
-    <span className="marquee--text--universe">UNIVERSE</span>
-    <span className="marquee--text--polymorph">POLYMORPH</span>
-    <span className="marquee--text--universe">UNIVERSE</span>
-    <span className="marquee--text--polymorph">POLYMORPH</span>
-    <span className="marquee--text--universe">UNIVERSE</span>
-    <span className="marquee--text--polymorph">POLYMORPH</span>
-    <span className="marquee--text--universe">UNIVERSE</span>
-    <span className="marquee--text--polymorph">POLYMORPH</span>
-    <span className="marquee--text--universe">UNIVERSE</span>
-    <span className="marquee--text--polymorph">POLYMORPH</span>
-    <span className="marquee--text--universe">UNIVERSE</span>
-    <span className="marquee--text--polymorph">POLYMORPH</span>
-    <span className="marquee--text--universe">UNIVERSE</span>
-    <span className="marquee--text--polymorph">POLYMORPH</span>
-    <span className="marquee--text--universe">UNIVERSE</span>
-  </p>
-);
+const Welcome = () => {
+  const [animation, setAnimation] = useState();
+  const windowSize = useWindowSize();
 
-const Welcome = () => (
-  <div className="welcome--section--rarity--charts">
-    <WelcomeWrapper
-      title="Polymorph Rarity Chart"
-      hintText="10,000 Total Polymorphs"
-      ellipsesLeft={false}
-      ellipsesRight={false}
-      // marquee={marquee()}
-      bgTextRight
-    />
-  </div>
-);
+  useEffect(() => {
+    if (+windowSize.width <= 575) setAnimation(animationDataMobile);
+    if (+windowSize.width <= 1024) setAnimation(animationDataTablet);
+    else setAnimation(animationDataDesktop);
+  }, [windowSize.width]);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className="welcome--section--rarity--charts">
+      <div className={"rarity--background--animation"}>
+        <Lottie options={defaultOptions} isClickToPauseDisabled />
+      </div>
+      <WelcomeWrapper
+        title="Polymorph Rarity Chart"
+        hintText="10,000 Total Polymorphs"
+        ellipsesLeft={false}
+        ellipsesRight={false}
+        // marquee={marquee()}
+        bgTextRight
+      />
+    </div>
+  );
+};
 
 export default Welcome;
