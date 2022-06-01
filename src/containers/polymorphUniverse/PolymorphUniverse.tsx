9c21888e9f972cb936887a1ad39e305f46e2337b
gope-gope <88503390+gope-gope@users.noreply.github.com>
2022-06-01 09:25:55 +0300

    Burn-to-mint page using ISSG (#83)

--- src/containers/polymorphUniverse/PolymorphUniverse.tsx
+++ src/containers/polymorphUniverse/PolymorphUniverse.tsx
@@ -2,50 +2,26 @@ import React, { useEffect, useState } from 'react';
 // import './PolymorphUniverse.scss';
 import BurnToMint from '../polymorphs/BurnToMint';
 import WhatsNewSection from '../polymorphs/WhatsNewSection';
-
 import { GetStaticProps } from 'next';
-import { dehydrate, QueryClient } from 'react-query';
-import { GetCollectionApi, GetNFT2Api } from '@app/modules/nft/api';
-import { collectionKeys, nftKeys } from '@app/utils/query-keys';
-import Contracts from "../../contracts/contracts.json";
-import { ethers } from "ethers";
-import { useContractsStore } from "src/stores/contractsStore";
-import { usePolymorphStore } from 'src/stores/polymorphStore';
+import { queryPolymorphsGraphV2 } from '@legacy/graphql/polymorphQueries';
+import { burnedPolymorphs } from "@legacy/graphql/polymorphQueries";
 
-export const PolymorphUniverse = ({} : any) => {
-  const { totalBurnedPolymorphs} = usePolymorphStore();
-
+export const PolymorphUniverse = ({burntPolymorphs} : any) => {
   return (
     <div className="polymorph--universe--general--page">
-      <BurnToMint burntCount={totalBurnedPolymorphs?.length}/>
+      <BurnToMint burntCount={burntPolymorphs.burnedEntities?.length}/>
       <WhatsNewSection />
     </div>
   )
 };
 
-
-// TO DO:
-// Re-write this when we have an instance 
-// of the V2 contract live on test net.
+// NextJs ISSG Page, rebuilt every 60 seconds.
+// A rebuild is trigger by a page visit.
 export const getStaticProps: GetStaticProps = async () => {
-  const queryClient = new QueryClient();
-  const collectionAddress = '0x1cBB182322Aee8ce9F4F1f98d7460173ee30Af1F'
-  const tokenId = '123';
-  await queryClient.prefetchQuery(nftKeys.nftInfo({collectionAddress, tokenId}), async () => {
-    const result = await GetNFT2Api(collectionAddress, tokenId, false);
-    // Dehydration will fail if there's a Date or undefined value in the NFT model
-    // This will strip any invalid values
-    return JSON.parse(JSON.stringify(result));
-  });
-
-  await queryClient.prefetchQuery(collectionKeys.centralizedInfo(collectionAddress), async () => {
-    const result = await GetCollectionApi(collectionAddress);
-    return JSON.parse(JSON.stringify(result));
-  });
-
+  const burned = await queryPolymorphsGraphV2(burnedPolymorphs);
   return {
     props: {
-      dehydratedState: dehydrate(queryClient),
+      burntPolymorphs: burned,
     },
     revalidate: 60
   };
