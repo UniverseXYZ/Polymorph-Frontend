d2caa75ddb02db8740fc4bfc862d63171ba36da5
gope-gope <georgigeorgiew97@gmail.com>
2022-06-23 09:49:49 +0300

    Fixed open graph import

--- src/containers/polymorphicFaces/SinglePolymorphicFaceDetails.jsx
+++ src/containers/polymorphicFaces/SinglePolymorphicFaceDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { OpenGraph } from "@app/components";
+import { OpenGraph } from "../../components/open-graph";
 import ImageWithBadges from "../../components/polymorphicFaces/singlePolymorphicFaceDetails/ImageWithBadges";
 import DetailsWithTabs from "../../components/polymorphicFaces/singlePolymorphicFaceDetails/DetailsWithTabs";
 import LoadingSpinner from "../../components/svgs/LoadingSpinner.jsx";
