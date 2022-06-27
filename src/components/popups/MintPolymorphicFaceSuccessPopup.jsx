57957019d3aa70eb19a3eb56d9d0225562dc98e9
gope-gope <georgigeorgiew97@gmail.com>
2022-06-27 13:30:47 +0300

    Added success popup to claim section

---
+++ src/components/popups/MintPolymorphicFaceSuccessPopup.jsx
@@ -0,0 +1,54 @@
+import PropTypes from "prop-types";
+import uuid from "react-uuid";
+import Button from "../button/Button.jsx";
+import closeIcon from "../../assets/images/cross.svg";
+import { useMyNftsStore } from "src/stores/myNftsStore";
+import Image from "next/image.js";
+import SuccessBubble from "../../assets/images/successful-claim-bubble.png";
+
+const MintPolymorphConfirmationPopup = ({ amount, onClose }) => {
+  return (
+    <div className="polymorph_popup">
+      <img
+        className="close"
+        src={closeIcon}
+        alt="Close"
+        onClick={onClose}
+        aria-hidden="true"
+      />
+      <Image
+        src={SuccessBubble}
+        height={128}
+        width={128}
+        style={{ marginTop: "32px", marginBottom: "32px" }}
+      />
+      <h1>Congratulations!</h1>
+      <p className="desc">
+        {amount === 1
+          ? `You have successfully claimed your Polymorphic Face. It will appear in your wallet soon.
+        `
+          : `You have successfully claimed ${amount} Polymorphic Faces. They will appear in your wallet soon.`}
+      </p>
+      <p className="desc">
+        Transaction hash:{" "}
+        <span
+          className="txHash"
+          onClick={() => window.open(`https://etherscan.io/`)}
+        >
+          {"TxHash"}
+        </span>
+      </p>
+      <div className="button__div_polymorph">
+        <Button className="light-border-button" onClick={onClose}>
+          Close
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+MintPolymorphConfirmationPopup.propTypes = {
+  amount: PropTypes.number.isRequired,
+};
+
+export default MintPolymorphConfirmationPopup;
