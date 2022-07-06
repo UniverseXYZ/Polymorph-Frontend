84200a2b58fd00c3b667698e5af626c294cf4913
gope-gope <georgigeorgiew97@gmail.com>
2022-07-06 13:58:38 +0300

    Show no faces when the user owns none

--- src/utils/hooks/useMyFacesRarityDebouncer.js
+++ src/utils/hooks/useMyFacesRarityDebouncer.js
@@ -69,9 +69,9 @@ const buildRarityUrl = (
   if (ids.length) {
     endpoint = `${endpoint}&ids=${ids.join(',')}`;
   } 
-  // else {
-  //   endpoint = `${endpoint}&ids=1000000`;
-  // }
+  else {
+    endpoint = `${endpoint}&ids=1000000`;
+  }
 
   return endpoint;
 };
