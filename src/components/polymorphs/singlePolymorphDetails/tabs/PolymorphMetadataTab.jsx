db8844c62e743dfd2a4edda0fb1fa9f73837eef3
gope-gope <georgigeorgiew97@gmail.com>
2022-08-09 15:41:46 +0300

    Adjusted styling of metadata tab

--- src/components/polymorphs/singlePolymorphDetails/tabs/PolymorphMetadataTab.jsx
+++ src/components/polymorphs/singlePolymorphDetails/tabs/PolymorphMetadataTab.jsx
@@ -2,24 +2,52 @@ import React from "react";
 import PolymorphMetadataCard from "../../polymorphMetadataCard/PolymorphMetadataCard";
 import { useContractsStore } from "src/stores/contractsStore";
 
-const PolymorphMetadataTab = ({ morphPrice, owner, genome }) => {
+const PolymorphMetadataTab = ({
+  morphPrice,
+  owner,
+  genome,
+  contractAddress,
+  tokenId,
+  tokenStandard,
+  network,
+}) => {
   // Dummy data
   const polymorphMetadata = [
     {
       id: 1,
-      label: "Next morph price",
-      price: morphPrice,
-      address: null,
+      label: "Contract Address",
+      address: contractAddress,
     },
     {
       id: 2,
+      label: "Token ID",
+      value: tokenId,
+    },
+    {
+      id: 3,
+      label: "Token Standard",
+      value: tokenStandard,
+    },
+    {
+      id: 4,
+      label: "Blockchain",
+      value: network,
+    },
+    {
+      id: 5,
       label: "Owner",
       address: owner,
     },
     {
-      id: 3,
+      id: 6,
       label: "Genome string",
       address: genome,
+    },
+    {
+      id: 7,
+      label: "Next morph price",
+      price: morphPrice,
+      address: null,
     },
   ];
 
